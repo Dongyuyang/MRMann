672505ecb48b1ae1594f83115a2fb40e2b4fcd81
tou <dse2015@dses-MacBook-Pro.local>
2015-08-20 18:18:05 +0900

    revise

--- MRMann.hpp
+++ MRMann.hpp
@@ -1,6 +1,21 @@
 #include "../commonTool/common_tool.hpp"
+#include "Rtree.h"
 #include <cmath>
 
+std::vector<std::vector<double> > get_outer_square_mbr_sqrt_eight(const std::vector<std::vector<double> > &mbr)
+{
+  double factor = 1 / std::sqrt(8);
+  double edge = std::abs(mbr[1][0] - mbr[0][0]);
+  double delta = 0.5*edge - 0.5*factor*edge;
+  std::vector<double> new_low;
+  std::vector<double> new_up;
+  for(int i = 0; i < mbr[0].size();i++){
+    new_low.push_back(mbr[0][i] - delta);
+    new_up.push_back(mbr[1][i] + delta);
+  }
+  return {new_low,new_up};
+}
+  
 std::vector<std::vector<double> > get_inner_square_mbr_sqrt_eight(const std::vector<std::vector<double> > &mbr)
 {
   double factor = 1 / std::sqrt(8);
@@ -15,7 +30,6 @@ std::vector<std::vector<double> > get_inner_square_mbr_sqrt_eight(const std::vec
     new_low.push_back(mbr[0][i] + delta);
     new_up.push_back(mbr[1][i] - delta);
   }
-
   return {new_low,new_up};
 }
 
@@ -26,4 +40,19 @@ bool if_point_in_mbr(const std::vector<double> &p, const std::vector<std::vector
       return false;
   }
   return true;
+}
+
+int MRM_query(const std::vector<std::vector<double> > &points, std::vector<std::vector<double> > qs, bgi::rtree<Value, bgi::rstar<16> > &rtree)
+{
+  std::vector<std::vector<double> > mbr = get_mbr(qs);
+  std::vector<int> query_result;
+  while(true){
+    query_result = range_search(rtree,mbr);
+    if(query_result.empty()){
+      break;
+    }else{
+      
+    }
+    
+  }
 }
